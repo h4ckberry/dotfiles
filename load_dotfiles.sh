2358896363d7fa0b6a3aa32dab9a7f11c0674bd9
koooota <eno6k21@gmail.com>
2021-09-18 18:15:06 +0900

    fix load dotfiles

--- load_dotfiles.sh
+++ load_dotfiles.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
-
 echo LOAD DOT FILES
+
+select dotfile in '.vimrc' '.bashrc' '.tmux.conf'
+do
+  echo "select $dotfile"
+  break
+done
+
+if [ "$dotfile" = ".vimrc" ]; then
+  source "$(curl -s https://raw.githubusercontent.com/koooota/dotfiles/master/.vimrc)"
+elif [ "$dotfile" = ".bashrc" ]; then
+  bash -c "$(curl -s https://raw.githubusercontent.com/koooota/dotfiles/master/.bashrc)"
+elif [ "$dotfile" = ".tmux.conf" ]; then
+  tmux source-file "$(curl -ssource-file https://raw.githubusercontent.com/koooota/dotfiles/master/.tmux.conf)"
+else
+  echo "Select the displayed number"
+fi
